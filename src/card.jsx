367260fe2711b813d5911c1ebebe9d19b2ca9105
Huy Nguyen <huy.h.nguy@gmail.com>
2024-01-21 15:45:26 -0600

    add styling to cards and have cards displayed evenly around the page

--- src/card.jsx
+++ src/card.jsx
@@ -1,7 +1,59 @@
-import { createApi } from "unsplash-js"
 import { useState, useEffect } from "react";
 
-export default function Card({id}) {
+function Image({ id }) {
+    const [url, setUrl] = useState('');
+
+    const firstHalf = 'https://api.disneyapi.dev/character/';
+    let characterInfo = firstHalf.concat(id);
+
+    useEffect(() => {
+        fetch(characterInfo, {mode: 'cors'})
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(response) {
+                const link = `${response.data.imageUrl}`;
+                setUrl(link);
+            });
+    }, []);
+    
+    return (
+        <img src={url}>
+        </img>
+    )
+}
+
+function Name({ id }) {
+    const [name, setName] = useState('');
+
+    const firstHalf = 'https://api.disneyapi.dev/character/';
+    let characterInfo = firstHalf.concat(id);
+
+    useEffect(() => {
+        fetch(characterInfo, {mode: 'cors'})
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(response) {
+                setName(response.data.name);
+            });
+    }, []);
+
+    return (
+        <h2>{name}</h2>
+    )
+}
+
+export default function Card ({ id }) {
+    return (
+        <div className = "card">
+            <Image id = {id} />
+            <Name id = {id} />
+        </div>
+    )
+}
+
+/*export default function Card({id}) {
     const [url, setUrl] = useState('');
     const [name, setName] = useState('');
 
@@ -29,4 +81,4 @@ export default function Card({id}) {
         </>
            
     )
-}
+}*/
